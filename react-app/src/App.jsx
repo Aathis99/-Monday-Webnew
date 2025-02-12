aa0819146fcd7dc82ac1c39374a6cabc08760c1b
Aathis99 <132294598+Aathis99@users.noreply.github.com>
2025-02-12 21:29:45 +0700

    Update App.jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,16 +1,29 @@
-// import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <>
-      <Container>
-        <Row>
-          <Col sm={6}>Hello</Col>
-          <Col sm={6}>World</Col>
-        </Row>
-      </Container>
+      <div>
+        <Container>
+          <Row>
+            <Col sm={6}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src="holder.js/100px180px" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Ratione, ea? Nisi temporibus vel voluptates obcaecati fugit
+                    aliquid, magnam et a.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 }
